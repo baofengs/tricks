59c1df594466e8584cf089e4afbd17535f9a4691
bao <baoyeyene@gmail.com>
2019-01-30 13:15:40 +0800

    update source

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,19 @@
 #!/usr/bin/env sh
 
-# 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件
 npm run docs:build
 
-# 进入生成的文件夹
 cd ./.vuepress/dist
-
-# 如果是发布到自定义域名
 echo 'tricks.sanbaofengs.com' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
-
-# 更新编译后的页面
 git push -f git@github.com:x-bao/tricks.git master:gh-pages
-
-echo 'deploy pages done'
+echo '=== deploy pages done ==='
 
 cd -
-
 git add -A
 git commit -m 'update source'
-
-# 更新原文
 git push -f git@github.com:x-bao/tricks.git master:master
+echo '=== sync source code done ==='
